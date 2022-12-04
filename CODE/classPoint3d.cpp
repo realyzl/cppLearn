1e8beac11797bd435d061bb3e6322ca7d4e7d9b0
realyzl <realyzl@outlook.com>
2022-12-04 12:18:51 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   .vscode/c_cpp_properties.json
    	modified:   CODE/ReadInput.exe
    	modified:   CODE/TheFirstC++Program.exe
    	new file:   CODE/classPoint3d.cpp

---
+++ CODE/classPoint3d.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    class classPoint3d
+    {
+    private:
+        float x,y,z;
+    public:
+        classPoint3d(float x,float y,float z);
+        print(){
+            cout<<1;
+        };
+    };
+    classPoint3d a(1,2,3);
+    a.print;
+    
+    
+}
